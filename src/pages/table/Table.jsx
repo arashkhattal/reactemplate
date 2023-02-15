bb84e688943c868e036ad05c9056e7fdfab6a15b
arash-oyesters <arash@oyesters.in>
2023-02-15 22:45:29 +0530

    arash - added commenty in scroll bar

--- src/pages/table/Table.jsx
+++ src/pages/table/Table.jsx
@@ -8,6 +8,7 @@ import { TextField } from "@mui/material";
 import Outlined from "./tableData/Outlined";
 import Filled from "./tableData/Filled";
 import Standard from "./tableData/Standard";
+import { Card } from "@material-ui/core";
 
 const Table = () => {
   const [value, setValue] = React.useState("1");
@@ -17,74 +18,68 @@ const Table = () => {
   };
 
   return (
-    <Box
-    className="main_content"
-      sx={{ width: "100%", typography: "body1" }}
-    >
-      <TabContext value={value}>
-        <Box
-          className="global_display_flex"
-          sx={{
-            borderBottom: 1,
-            borderColor: "divider",
-          }}
-        >
-          <TabList
-            onChange={handleChange}
-            aria-label="lab API tabs example"
-          >
-            <Tab label="Outlined" value="1" />
-            <Tab label="Filled" value="2" />
-            <Tab label="Standard" value="3" />
-          </TabList>
-        </Box>
-        <TabPanel value="1">
+      <Box className="main_content"  sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
           <Box
-            component="form"
+            className="global_display_flex"
             sx={{
-              "& .MuiTextField-root": {
-                m: 1,
-                width: "25ch",
-              },
+              borderBottom: 1,
+              borderColor: "divider",
             }}
-            noValidate
-            autoComplete="off"
           >
-            <Outlined />
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Outlined" value="1" />
+              <Tab label="Filled" value="2" />
+              <Tab label="Standard" value="3" />
+            </TabList>
           </Box>
-        </TabPanel>
-        <TabPanel value="2">
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": {
-                m: 1,
-                width: "25ch",
-              },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <Filled />
-          </Box>
-        </TabPanel>
-        <TabPanel value="3">
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": {
-                m: 1,
-                width: "25ch",
-              },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <Standard />
-          </Box>
-        </TabPanel>
-      </TabContext>
-    </Box>
+          <TabPanel value="1">
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": {
+                  m: 1,
+                  width: "25ch",
+                },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <Outlined />
+            </Box>
+          </TabPanel>
+          <TabPanel value="2">
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": {
+                  m: 1,
+                  width: "25ch",
+                },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <Filled />
+            </Box>
+          </TabPanel>
+          <TabPanel value="3">
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": {
+                  m: 1,
+                  width: "25ch",
+                },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <Standard />
+            </Box>
+          </TabPanel>
+        </TabContext>
+      </Box>
   );
 };
 export default Table;
