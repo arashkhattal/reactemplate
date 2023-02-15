3acb747a50325e00e26c766d271a49dd36b423e8
arash-oyesters <arash@oyesters.in>
2023-02-15 13:06:35 +0530

    arash - added dynamic layout

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -7,15 +7,7 @@ const AppBar = () => {
     <>
       {/* simple app bar contain 3 options */}
       <div className="navbar">
-        <a href="#">
-          <i className="fa fa-fw fa-home"></i> Home
-        </a>
-        <a href="#">
-          <i className="fa fa-fw fa-search"></i> Search
-        </a>
-        <a href="#">
-          <i className="fa fa-fw fa-envelope"></i> Contact
-        </a>
+        
       </div>
     </>
   );
