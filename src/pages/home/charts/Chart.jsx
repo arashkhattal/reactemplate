fa7ebcf32fc8f2f1444b1cb3ca31d4a1c5909838
arash-oyesters <arash@oyesters.in>
2023-02-16 12:58:59 +0530

    arash - added icons

---
+++ src/pages/home/charts/Chart.jsx
@@ -0,0 +1,25 @@
+import { Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useGlobalContext } from "../../../context/globalContext";
+
+const Contact = () => {
+  const { setLoading } = useGlobalContext();
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 500);
+  }, []);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 4000);
+  return (
+    <>
+      <div>
+        <Typography variant="h2">Chart Page</Typography>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
