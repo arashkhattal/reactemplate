13fb81d1f6aa1ea7fddb36eeed56d8dbe1fa445d
jabed-Oyesters <jabed@oyesters.in>
2023-02-20 12:34:08 +0600

    Jabed - Added Reset Modal Password

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,8 @@
 // @mui material components
 import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
 import React, {
-  useState,
-  useEffect,
+    useState,
+    useEffect,
 } from "react";
 // Data
 import { Card, TextField } from "@mui/material";
@@ -10,262 +10,277 @@ import { useGlobalContext } from "../../context/globalContext";
 
 import { Box } from "@mui/system";
 import {
-  validateEmail,
-  validatePhone,
+    validateEmail,
+    validatePhone,
 } from "../../helpers/globalFunction";
+import ResetPasswordModal from "./ResetPasswordModal";
 
 function Profile() {
-  // store email
-  const [email, setEmail] = useState(
-    "jabed@gmail.com"
-  );
-  //   store name
-  const [fullName, setFullName] =
-    useState("Jabed");
-  // store phone number
-  const [phoneno, setPhoneno] = useState(
-    "01789274185"
-  );
-  //   store address
-  const [address, setAdders] = useState(
-    "Dhaka, Bangladesh"
-  );
-  //   selected photo
-  const [selectedFile, setSelectedFile] =
-    useState(null);
-  // preview photo
-  const [preview, setPreview] = useState(null);
-  console.log(selectedFile);
-  // global context
-  const { setResetModal, setAlert } =
-    useGlobalContext();
-  // make URL fOR PREVIEW
-  useEffect(() => {
-    if (!selectedFile) {
-      //  setPreview(undefined);
-      return;
-    }
-    const objectUrl =
-      URL.createObjectURL(selectedFile);
-    setPreview(objectUrl);
-    // free memory when ever this component is unmounted
-    return () => URL.revokeObjectURL(objectUrl);
-  }, [selectedFile]);
+    // store email
+    const [email, setEmail] = useState(
+        "jabed@gmail.com"
+    );
+    //   store name
+    const [fullName, setFullName] =
+        useState("Jabed");
+    // store phone number
+    const [phoneno, setPhoneno] = useState(
+        "01789274185"
+    );
+    //   store address
+    const [address, setAdders] = useState(
+        "Dhaka, Bangladesh"
+    );
+    //   selected photo
+    const [selectedFile, setSelectedFile] =
+        useState(null);
+    // preview photo
+    const [preview, setPreview] = useState(null);
+    console.log(selectedFile);
+    // global context
+    const { setResetModal, setAlert } =
+        useGlobalContext();
+    // make URL fOR PREVIEW
+    useEffect(() => {
+        if (!selectedFile) {
+            //  setPreview(undefined);
+            return;
+        }
+        const objectUrl =
+            URL.createObjectURL(selectedFile);
+        setPreview(objectUrl);
+        // free memory when ever this component is unmounted
+        return () =>
+            URL.revokeObjectURL(objectUrl);
+    }, [selectedFile]);
 
-  //   select photo function
-  const onSelectFile = (e) => {
-    if (
-      !e.target.files ||
-      e.target.files.length === 0
-    ) {
-      setSelectedFile(undefined);
-      return;
-    }
+    //   select photo function
+    const onSelectFile = (e) => {
+        if (
+            !e.target.files ||
+            e.target.files.length === 0
+        ) {
+            setSelectedFile(undefined);
+            return;
+        }
 
-    setSelectedFile(e.target.files[0]);
-  };
-  // submit function
-  const handleSubmit = () => {
-    if (fullName === "") {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please enter Full Name",
-      });
-      return;
-    }
-    if (email === "") {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please enter Email",
-      });
-      return;
-    }
-    if (!validateEmail(email)) {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please enter Valid Email",
-      });
-      return;
-    }
+        setSelectedFile(e.target.files[0]);
+    };
+    // submit function
+    const handleSubmit = () => {
+        if (fullName === "") {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please enter Full Name",
+            });
+            return;
+        }
+        if (email === "") {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please enter Email",
+            });
+            return;
+        }
+        if (!validateEmail(email)) {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please enter Valid Email",
+            });
+            return;
+        }
 
-    if (!validatePhone(phoneno)) {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please enter Valid Phone Number",
-      });
-      return;
-    }
-    if (selectedFile === "") {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please Select Image",
-      });
-      if (address === "") {
-        setAlert({
-          flag: true,
-          type: "error",
-          msg: "Please enter Address",
-        });
-        return;
-      }
-      return;
-    } else {
-      setAlert({
-        flag: true,
-        type: "success",
-        msg: "Update successful",
-      });
-      console.log(
-        `Image name: ${selectedFile?.name}Image URL: ${preview}`,
-        email,
-        fullName,
-        phoneno,
-        address
-      );
-    }
-  };
-
-  return (
-    <>
-      {/* <ResetPasswordModal /> */}
-      <Card
-        sx={{
-          padding: "20px",
-          margin: "15px 5px",
-        }}
-      >
-        <Box mb={2} />
-        <div
-          sx={{
-            display: "inline-block",
-            alignItems: "center",
-          }}
-        >
-          <img
-            alt=""
-            src={
-              preview ||
-              "https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg"
+        if (!validatePhone(phoneno)) {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please enter Valid Phone Number",
+            });
+            return;
+        }
+        if (selectedFile === "") {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please Select Image",
+            });
+            if (address === "") {
+                setAlert({
+                    flag: true,
+                    type: "error",
+                    msg: "Please enter Address",
+                });
+                return;
             }
-            style={{
-              marginTop: "20px",
-              width: "70px",
-              height: "70px",
-              fontSize: "50px",
-              borderRadius: "50%",
-              marginLeft: "44%",
-            }}
-          ></img>
+            return;
+        } else {
+            setAlert({
+                flag: true,
+                type: "success",
+                msg: "Update successful",
+            });
+            console.log(
+                `Image name: ${selectedFile?.name}Image URL: ${preview}`,
+                email,
+                fullName,
+                phoneno,
+                address
+            );
+        }
+    };
 
-          <label>
-            <input
-              type="file"
-              onChange={onSelectFile}
-              accept=".png,.jpg,.jpeg"
-              style={{
-                display: "none",
-              }}
-            />
-            <ModeEditOutlineIcon
-              style={{ cursor: "pointer" }}
-            />
-          </label>
-        </div>
-
-        <Box mt={5} mb={3}>
-          <Box
-            sx={{
-              display: "grid",
-              gridTemplateColumns: "1fr 1fr",
-              gap: "30px",
-            }}
-          >
-            <TextField
-              size="small"
-              label="Full Name"
-              type="text"
-              fullWidth
-              value={fullName}
-              onChange={(e) =>
-                setFullName(e.target.value)
-              }
-            ></TextField>
-
-            <TextField
-              label="Email"
-              size="small"
-              type="text"
-              fullWidth
-              value={email}
-              onChange={(e) =>
-                setEmail(e.target.value)
-              }
-            ></TextField>
-
-            <TextField
-              label="Phone No"
-              size="small"
-              type="number"
-              fullWidth
-              value={phoneno}
-              onChange={(e) =>
-                setPhoneno(e.target.value)
-              }
-            />
-
-            <TextField
-              label="Address"
-              size="small"
-              type="text"
-              fullWidth
-              value={address}
-              onChange={(e) =>
-                setAdders(e.target.value)
-              }
-            />
-          </Box>
-          <Box
-            pt={3}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              gap: "10px",
-            }}
-          >
-            <button
-              className="btn_primary warning_btn "
-              style={{
-                color: "white",
-                width: "15%",
-              }}
-              size="small"
-              onClick={() => setResetModal(true)}
+    return (
+        <>
+            <ResetPasswordModal />
+            <Card
+                style={{
+                    padding: "20px",
+                    margin: "15px 5px",
+                }}
             >
-              Reset Password
-            </button>
-            <button
-              className="btn_primary btn_primary_hover"
-              size="small"
-              style={{
-                color: "white",
-                width: "15%",
-              }}
-              onClick={() => {
-                handleSubmit();
-              }}
-            >
-              Save
-            </button>
-          </Box>
-        </Box>
-      </Card>
-    </>
-  );
+                <Box mb={2} />
+                <div>
+                    <img
+                        alt=""
+                        src={
+                            preview ||
+                            "https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg"
+                        }
+                        style={{
+                            marginTop: "20px",
+                            width: "70px",
+                            height: "70px",
+                            fontSize: "50px",
+                            borderRadius: "50%",
+                            marginLeft: "44%",
+                        }}
+                    ></img>
+
+                    <label>
+                        <input
+                            type="file"
+                            onChange={
+                                onSelectFile
+                            }
+                            accept=".png,.jpg,.jpeg"
+                            style={{
+                                display: "none",
+                            }}
+                        />
+                        <ModeEditOutlineIcon
+                            style={{
+                                cursor: "pointer",
+                            }}
+                        />
+                    </label>
+                </div>
+
+                <Box mt={5} mb={3}>
+                    <Box
+                        style={{
+                            display: "grid",
+                            gridTemplateColumns:
+                                "1fr 1fr",
+                            gap: "30px",
+                        }}
+                    >
+                        <TextField
+                            size="small"
+                            label="Full Name"
+                            type="text"
+                            fullWidth
+                            value={fullName}
+                            onChange={(e) =>
+                                setFullName(
+                                    e.target.value
+                                )
+                            }
+                        ></TextField>
+
+                        <TextField
+                            label="Email"
+                            size="small"
+                            type="text"
+                            fullWidth
+                            value={email}
+                            onChange={(e) =>
+                                setEmail(
+                                    e.target.value
+                                )
+                            }
+                        ></TextField>
+
+                        <TextField
+                            label="Phone No"
+                            size="small"
+                            type="number"
+                            fullWidth
+                            value={phoneno}
+                            onChange={(e) =>
+                                setPhoneno(
+                                    e.target.value
+                                )
+                            }
+                        />
+
+                        <TextField
+                            label="Address"
+                            size="small"
+                            type="text"
+                            fullWidth
+                            value={address}
+                            onChange={(e) =>
+                                setAdders(
+                                    e.target.value
+                                )
+                            }
+                        />
+                    </Box>
+                    <Box
+                        pt={3}
+                        style={{
+                            display: "flex",
+                            justifyContent:
+                                "center",
+                            gap: "10px",
+                        }}
+                    >
+                        <button
+                            className="btn_primary warning_btn "
+                            style={{
+                                color: "white",
+                                width: "15%",
+                            }}
+                            size="small"
+                            onClick={() =>
+                                setResetModal(
+                                    true
+                                )
+                            }
+                        >
+                            Reset Password
+                        </button>
+                        <button
+                            className="btn_primary btn_primary_hover"
+                            size="small"
+                            style={{
+                                color: "white",
+                                width: "15%",
+                            }}
+                            onClick={() => {
+                                handleSubmit();
+                            }}
+                        >
+                            Save
+                        </button>
+                    </Box>
+                </Box>
+            </Card>
+        </>
+    );
 }
 
 export default Profile;
--- src/pages/Profile/ResetPasswordModal.jsx
+++ src/pages/Profile/ResetPasswordModal.jsx
@@ -1,184 +1,183 @@
-import { Card, Divider, Grid, Modal } from "@mui/material";
+import Typography from "@material-ui/core/Typography";
+import {
+    Box,
+    Card,
+    Divider,
+    Grid,
+    Modal,
+    TextField,
+} from "@mui/material";
 
-import { useGlobalContext } from "context/globalContext";
 import React, { useState } from "react";
-// import axiosInstance from "helpers/axiosInstance";
+import { useGlobalContext } from "../../context/globalContext";
+import "./profile.css";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  width: "100%",
-  maxWidth: "600px",
-  transform: "translate(-50%, -50%)",
-  boxShadow: 24,
-  borderRadius: 2,
-  p: 4,
-};
 const ResetPasswordModal = () => {
-  const {
-    reload,
-    setReload,
-    setAlert,
-    setLoading,
-    resetModal,
-    setResetModal,
-    handle203,
-  } = useGlobalContext();
-  const [currentpass, setCurrentpass] = useState("");
-  const [newpass, setNewpass] = useState("");
-  const [repass, setRepass] = useState("");
-  const handlesubmit = async (e) => {
-    if (currentpass === "") {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please enter Current Password",
-      });
-      return;
-    }
-    if (newpass === "") {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please Enter New Password",
-      });
-      return;
-    }
-    if (repass === "") {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please Re-enter New Password",
-      });
-      return;
-    }
+    // store current password
+    const [currentPass, setCurrentPass] =
+        useState("");
+    // store new password
+    const [newPass, setNewPass] = useState("");
+    // store
+    const [confirmPass, setConfirmPass] =
+        useState("");
+    // global function
+    const {
+        resetModal,
+        setResetModal,
+        setAlert,
+    } = useGlobalContext();
 
-    if (newpass !== repass) {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Please Check Your New Password & Confirm Password",
-      });
-      return;
-    }
-    try {
-      setLoading(true);
-      const response = await axiosInstance.post(
-        "/backend/change_super_admin_password",
-        {
-          old_password: currentpass,
-          new_password: newpass,
+    // submit password function
+    const handleSubmit = async (e) => {
+        if (currentPass === "") {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please enter Current Password",
+            });
+            return;
         }
-      );
-      if (response?.status === 200) {
-        setAlert({
-          flag: true,
-          type: "success",
-          msg: response?.data?.msg,
-        });
-        setCurrentpass("");
-        setNewpass("");
-        setRepass("");
-        setReload(reload + 1);
+        if (newPass === "") {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please Enter New Password",
+            });
+            return;
+        }
+        if (confirmPass === "") {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please Re-enter New Password",
+            });
+            return;
+        }
+        if (newPass !== confirmPass) {
+            setAlert({
+                flag: true,
+                type: "error",
+                msg: "Please Check Your New Password & Confirm Password",
+            });
+            return;
+        } else {
+            setAlert({
+                flag: true,
+                type: "success",
+                msg: "Password changed successfully",
+            });
+            console.log(
+                `New Password is: ${newPass}`
+            );
+        }
         setResetModal(false);
-      } else if (response?.status === 203) {
-        handle203(response);
-      }
-      setLoading(false);
-    } catch (error) {
-      setAlert({
-        flag: true,
-        type: "error",
-        msg: "Something went wrong! please try again",
-      });
-      setLoading(false);
-    }
-  };
+        setCurrentPass("");
+        setNewPass("");
+        setConfirmPass("");
+    };
 
-  const boxSX = {
-    background: "#DC6C43",
-    color: "#ffffff",
-    marginLeft: 2,
-    "&:hover": {
-      color: "#ffffff",
-      backgroundColor: "#e37a54",
-    },
-  };
-
-  return (
-    <Modal open={resetModal} onClose={() => setResetModal(false)}>
-      <Card sx={style}>
-        <MDTypography
-          variant="h4"
-          style={{
-            textAlign: "center",
-          }}
-          fontWeight="medium"
+    return (
+        <Modal
+            open={resetModal}
+            onClose={() => setResetModal(false)}
         >
-          Reset Password
-        </MDTypography>
-        <Divider sx={{ mx: 0 }} />
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={12}>
-            <MDInput
-              label="Current Password"
-              size="medium"
-              type="password"
-              fullWidth
-              value={currentpass}
-              onChange={(e) => setCurrentpass(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12} md={12}>
-            <MDInput
-              label="New Password"
-              size="medium"
-              type="password"
-              fullWidth
-              value={newpass}
-              onChange={(e) => setNewpass(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12} md={12}>
-            <MDInput
-              label="Confirm Password"
-              size="medium"
-              type="password"
-              fullWidth
-              value={repass}
-              onChange={(e) => setRepass(e.target.value)}
-            />
-          </Grid>
-        </Grid>
-        <MDBox
-          pt={3}
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <MDButton
-            size="small"
-            color={"secondary"}
-            onClick={() => setResetModal(false)}
-          >
-            Cancel
-          </MDButton>
-          <MDButton
-            className="sendbtn1"
-            size="small"
-            sx={boxSX}
-            onClick={() => {
-              handlesubmit();
-            }}
-          >
-            Save
-          </MDButton>
-        </MDBox>
-      </Card>
-    </Modal>
-  );
+            <Card className="reset_modal_ui">
+                <Typography
+                    className="fs_24"
+                    style={{
+                        textAlign: "center",
+                        marginBottom: "10px",
+                    }}
+                    fontWeight="medium"
+                >
+                    Reset Password
+                </Typography>
+                <Divider sx={{ mx: 0 }} />
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={12}>
+                        <TextField
+                            label="Current Password"
+                            size="small"
+                            type="password"
+                            fullWidth
+                            value={currentPass}
+                            onChange={(e) =>
+                                setCurrentPass(
+                                    e.target.value
+                                )
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <TextField
+                            size="small"
+                            label="New Password"
+                            type="password"
+                            fullWidth
+                            value={newPass}
+                            onChange={(e) =>
+                                setNewPass(
+                                    e.target.value
+                                )
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <TextField
+                            label="Confirm Password"
+                            size="small"
+                            type="password"
+                            fullWidth
+                            value={confirmPass}
+                            onChange={(e) =>
+                                setConfirmPass(
+                                    e.target.value
+                                )
+                            }
+                        />
+                    </Grid>
+                </Grid>
+                <Box
+                    pt={3}
+                    style={{
+                        display: "flex",
+                        justifyContent:
+                            "space-between",
+                        gap: "10px",
+                    }}
+                >
+                    <button
+                        className="btn_primary warning_btn "
+                        style={{
+                            color: "white",
+                            width: "15%",
+                        }}
+                        size="small"
+                        color={"secondary"}
+                        onClick={() =>
+                            setResetModal(false)
+                        }
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        className="btn_primary btn_primary_hover"
+                        size="small"
+                        style={{
+                            color: "white",
+                            width: "15%",
+                        }}
+                        onClick={() => {
+                            handleSubmit();
+                        }}
+                    >
+                        Save
+                    </button>
+                </Box>
+            </Card>
+        </Modal>
+    );
 };
 
 export default ResetPasswordModal;
