e43e24d18c726e10d90f493789a2c00eb2ba6372
arash-oyesters <arash@oyesters.in>
2023-02-23 11:17:14 +0530

    arash - added card in suneditor

--- src/pages/sunEditor/SunEditor.jsx
+++ src/pages/sunEditor/SunEditor.jsx
@@ -1,3 +1,68 @@
+// import { Card, Typography } from "@mui/material";
+// import React, { useState } from "react";
+
+// // "npm i suneditor-react" - to install suneditor-react
+// import SunEditor from "suneditor-react";
+// import "suneditor/dist/css/suneditor.min.css";
+
+// function SunEditorPage() {
+//   // state to maintain the state of the editor data
+//   const [editorData, setEditorData] =
+//     useState("");
+//   return (
+//     <div>
+//       {/* card component from mui  */}
+//       <Card
+//         style={{
+//           padding: "10px",
+//         }}
+//       >
+//         {/* Typography component from MUI  */}
+//         <Typography
+//           className="global_display_flex fs_24 fw_600"
+//           style={{ marginBottom: "10px" }}
+//         >
+//           Sun Editor
+//         </Typography>
+//         {/* sun editor component for react */}
+//         <SunEditor
+//           setContents={editorData}
+//           onChange={(e) => setEditorData(e)}
+//           setOptions={{
+//             minHeight: "40vh",
+//             height: "auto",
+//             code: "en",
+//             buttonList: [
+//               ["undo", "redo"],
+//               [
+//                 "fontSize",
+//                 "bold",
+//                 "italic",
+//                 "underline",
+//                 "strike",
+//                 "subscript",
+//                 "superscript",
+//                 "fontColor",
+//                 "hiliteColor",
+//                 "align",
+//                 "outdent",
+//                 "indent",
+//                 "list",
+//                 "removeFormat",
+//               ],
+//               ["image", "table"],
+//               ["showBlocks", "codeView"],
+//             ],
+//             // plugins: plugins,
+//           }}
+//         />
+//       </Card>
+//     </div>
+//   );
+// }
+
+// export default SunEditorPage;
+
 import { Card, Typography } from "@mui/material";
 import React, { useState } from "react";
 
@@ -7,16 +72,12 @@ import "suneditor/dist/css/suneditor.min.css";
 
 function SunEditorPage() {
   // state to maintain the state of the editor data
-  const [editorData, setEditorData] =
-    useState("");
+  const [editorData, setEditorData] = useState("");
+
   return (
     <div>
       {/* card component from mui  */}
-      <Card
-        style={{
-          padding: "10px",
-        }}
-      >
+      <Card style={{ padding: "10px" }}>
         {/* Typography component from MUI  */}
         <Typography
           className="global_display_flex fs_24 fw_600"
@@ -57,6 +118,18 @@ function SunEditorPage() {
           }}
         />
       </Card>
+      {/* card to display editor data */}
+      {editorData.length === 0 ? (
+        ""
+      ) : (
+        <Card
+          style={{ marginTop: "15px" }}
+          className="global_card"
+          dangerouslySetInnerHTML={{
+            __html: editorData,
+          }}
+        ></Card>
+      )}
     </div>
   );
 }
