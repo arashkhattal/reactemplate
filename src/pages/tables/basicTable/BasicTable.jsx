fb9c9ea1b46ba2f03b1567de772a34024cecc875
arash-oyesters <arash@oyesters.in>
2023-02-23 13:44:34 +0530

    arash - added table with pagination , search

--- src/pages/tables/basicTable/BasicTable.jsx
+++ src/pages/tables/basicTable/BasicTable.jsx
@@ -1,155 +1,129 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import "../htmlTable/Table.css";
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TablePagination,
+} from "@material-ui/core";
+import { InputAdornment, TextField } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
-function createData(
-  name,
-  calories,
-  fat,
-  carbs,
-  protein
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-// table Data
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData(
-    "Ice cream sandwich",
-    237,
-    9.0,
-    37,
-    4.3
-  ),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+const columns = [
+  { id: "name", label: "Name", minWidth: 150 },
+  { id: "email", label: "Email", minWidth: 150 },
+  { id: "age", label: "Age", minWidth: 50 },
 ];
 
-export default function BasicTable() {
-  // state to maintain searh item
-  const [searchTerm, setSearchTerm] =
-    React.useState("");
+const data = [
+  { name: "John Doe", email: "johndoe@example.com", age: 25 },
+  { name: "Jane Doe", email: "janedoe@example.com", age: 30 },
+  { name: "Bob Smith", email: "bobsmith@example.com", age: 40 },
+  { name: "Alice Johnson", email: "alicejohnson@example.com", age: 35 },
+  { name: "Tom Brown", email: "tombrown@example.com", age: 45 },
+  { name: "Sara Lee", email: "saralee@example.com", age: 20 },
+  { name: "Peter Parker", email: "peterparker@example.com", age: 28 },
+  { name: "Mary Jane", email: "maryjane@example.com", age: 26 },
+  { name: "Clark Kent", email: "clarkkent@example.com", age: 33 },
+  { name: "Bruce Wayne", email: "brucewayne@example.com", age: 38 },
+];
 
-  // function to handle search
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+const BasicTable = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
   };
 
-  // Filter data based on search term
-  const filteredData = rows.filter((row) =>
-    Object.values(row).some((value) =>
-      value
-        .toString()
-        .toLowerCase()
-        .includes(searchTerm.toLowerCase())
-    )
-  );
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-  console.log(filteredData);
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    setPage(0);
+  };
+
+  const filteredData = data.filter((row) =>
+    Object.values(row)
+      .join(" ")
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <TableContainer>
+    <div>
       <div className="table-head">
         <div></div>
-        {/* search bar for table */}
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className="my-search-input"
-          placeholder="Search..."
+        <TextField
+          size="small"
+          variant="outlined"
+          placeholder="Search"
+          margin="dense"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
+          onChange={handleSearch}
         />
       </div>
-      {/* table component  */}
-      <Table
-        sx={{ minWidth: 650 }}
-        aria-label="simple table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              Dessert (100g serving)
-            </TableCell>
-            <TableCell align="right">
-              Calories
-            </TableCell>
-            <TableCell align="right">
-              Fat&nbsp;(g)
-            </TableCell>
-            <TableCell align="right">
-              Carbs&nbsp;(g)
-            </TableCell>
-            <TableCell align="right">
-              Protein&nbsp;(g)
-            </TableCell>
-            <TableCell align="right">
-              Status
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {filteredData.length === 0 ? (
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginLeft: "440px",
-              }}
-            >
-              <Typography
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  padding: "10px",
-                  margin: "50px",
-                  background: "lightgrey",
-                  textAlign: "center",
-                }}
-              >
-                {" "}
-                No Data
-              </Typography>
-            </div>
-          ) : (
-            filteredData.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{
-                  "&:last-child td, &:last-child th":
-                    { border: 0 },
-                }}
-              >
+      <TableContainer >
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
                 <TableCell
-                  component="th"
-                  scope="row"
+                  key={column.id}
+                  align="left"
+                  style={{ minWidth: column.minWidth }}
                 >
-                  {row.name}
+                  {column.label}
                 </TableCell>
-                <TableCell align="right">
-                  {row.calories}
-                </TableCell>
-                <TableCell align="right">
-                  {row.fat}
-                </TableCell>
-                <TableCell align="right">
-                  {row.carbs}
-                </TableCell>
-                <TableCell align="right">
-                  {row.protein}
-                </TableCell>
-              </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align="left">
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </div>
   );
-}
+};
+
+export default BasicTable;
