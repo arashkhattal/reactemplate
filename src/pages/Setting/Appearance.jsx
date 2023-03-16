226931d3af1378e8bcff6bb4899bb10502de2a12
arash-oyesters <arash@oyesters.in>
2023-03-16 12:40:32 +0530

    arash - added shadow color

--- src/pages/Setting/Appearance.jsx
+++ src/pages/Setting/Appearance.jsx
@@ -7,9 +7,11 @@ import {
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Setting = () => {
+  const [themeColor, setThemeColor] = useState("#0d80d8");
+
   useEffect(() => {
     const color = getComputedStyle(document.documentElement).getPropertyValue(
       "--color_primary"
@@ -17,9 +19,30 @@ const Setting = () => {
     console.log(`--color_primary: ${color}`);
   }, []);
 
-  function setColor(color) {
-    console.log(`Updating --color_primary to: ${color}`);
-    document.documentElement.style.setProperty("--color_primary", color);
+  function hexToRgba(hex, opacity) {
+    hex = hex.replace("#", "");
+    if (hex.length === 3) {
+      hex = hex
+        .split("")
+        .map(function (h) {
+          return h + h;
+        })
+        .join("");
+    }
+    var r = parseInt(hex.substring(0, 2), 16);
+    var g = parseInt(hex.substring(2, 4), 16);
+    var b = parseInt(hex.substring(4, 6), 16);
+    return "rgba(" + r + ", " + g + ", " + b + ", " + opacity + ")";
+  }
+
+  function setColor(themeColor) {
+    console.log(`Updating --color_primary to: ${themeColor}`);
+    const secondaryColor = hexToRgba(themeColor, 0.203);
+    document.documentElement.style.setProperty("--color_primary", themeColor);
+    document.documentElement.style.setProperty(
+      "--color_secondary",
+      secondaryColor
+    );
   }
 
   return (
@@ -40,8 +63,16 @@ const Setting = () => {
           </Box>
         </Box>
         <Divider sx={{ mx: 2.5, position: "relative", zIndex: "3" }} />
-        <Box>
+
+        <Box sx={{ mx: 3 }}>
           <Box mb={0.5} mt={3} ml={1}>
+            <Typography
+              variant="body2"
+              color="text"
+              style={{ padding: "10px 10px 10px 0px" }}
+            >
+              Pick a Default color
+            </Typography>
             <button
               style={{
                 width: "60px",
@@ -169,6 +200,29 @@ const Setting = () => {
               }}
               onClick={() => setColor("#FF0303")}
             ></button>
+            <div>
+              <Typography
+                variant="body2"
+                color="text"
+                style={{ padding: "10px 10px 10px 0px" }}
+              >
+                Choose custom color
+              </Typography>
+              <Icon fontSize="small" style={{ margin: "0px 6px 3px 0px" }}>
+                edit
+              </Icon>
+              <input
+              style={{cursor:'pointer',borderRadius:'5px'}}
+                type="color"
+                id="head"
+                name="head"
+                value={themeColor}
+                onChange={(e) => {
+                  setThemeColor(e.target.value);
+                  setColor(e.target.value);
+                }}
+              />
+            </div>
           </Box>
         </Box>
       </Card>
