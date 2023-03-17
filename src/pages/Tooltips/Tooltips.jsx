47c1fa10fb158e957a32034a2153a4c02fc95ff4
arash-oyesters <arash@oyesters.in>
2023-03-17 18:20:21 +0530

    arash - tooltip page

--- src/pages/Tooltips/Tooltips.jsx
+++ src/pages/Tooltips/Tooltips.jsx
@@ -1,78 +1,3 @@
-// import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Tooltip from "@material-ui/core/Tooltip";
-// import Button from "@material-ui/core/Button";
-// import IconButton from "@material-ui/core/IconButton";
-// import Typography from "@material-ui/core/Typography";
-// import { InfoOutlined, HelpOutline } from "@mui/icons-material";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: "flex",
-//     justifyContent: "space-around",
-//     alignItems: "center",
-//     height: "100vh",
-//   },
-//   button: {
-//     margin: theme.spacing(1),
-//   },
-// }));
-
-// const TooltipExample = () => {
-//   const classes = useStyles();
-
-//   return (
-//     <div className={classes.root}>
-//       <Tooltip title="Tooltip with default settings">
-//         <Button variant="contained" color="primary" className={classes.button}>
-//           Default Tooltip
-//         </Button>
-//       </Tooltip>
-//       <Tooltip title="Tooltip with arrow" arrow>
-//         <Button
-//           variant="contained"
-//           color="secondary"
-//           className={classes.button}
-//         >
-//           Arrow Tooltip
-//         </Button>
-//       </Tooltip>
-//       <Tooltip
-//         title="Tooltip with custom delay"
-//         enterDelay={500}
-//         leaveDelay={200}
-//       >
-//         <Button variant="outlined" color="primary" className={classes.button}>
-//           Custom Delay Tooltip
-//         </Button>
-//       </Tooltip>
-//       <Tooltip
-//         title={<Typography color="inherit">Tooltip with Typography</Typography>}
-//       >
-//         <Button variant="outlined" color="secondary" className={classes.button}>
-//           Typography Tooltip
-//         </Button>
-//       </Tooltip>
-//       <Tooltip title="Disabled Tooltip" disabled>
-//         <Button variant="contained" color="primary" className={classes.button}>
-//           Disabled Tooltip
-//         </Button>
-//       </Tooltip>
-//       <Tooltip title="Tooltip on icon button">
-//         <IconButton aria-label="info">
-//           <InfoOutlined />
-//         </IconButton>
-//       </Tooltip>
-//       <Tooltip title="Tooltip on help icon" placement="top-end">
-//         <IconButton aria-label="help">
-//           <HelpOutline />
-//         </IconButton>
-//       </Tooltip>
-//     </div>
-//   );
-// };
-
-// export default TooltipExample;
 
 import React from "react";
 
@@ -89,35 +14,36 @@ import { HelpOutline, InfoOutlined } from "@mui/icons-material";
 const TooltipExample = () => {
   return (
     <div>
-      <Card
-        style={{
-          padding: "20px",
-          margin: "20px",
-        }}
-      >
+      <Card className="global_card">
         <Grid container spacing={8}>
           <Grid item xs={12} md={6} lg={6}>
             <Typography className=" fs_14 fw_600">Default Tooltip</Typography>
             <Tooltip title="Tooltip with default settings">
-              <Button
-                variant="contained"
-                color="primary"
+              <button
+                className="btn_primary btn_primary_hover"
+                size="small"
+                style={{
+                  color: "white",
+                }}
                 //   className={classes.button}
               >
                 Default Tooltip
-              </Button>
+              </button>
             </Tooltip>
           </Grid>
           <Grid item xs={12} md={6} lg={6}>
             <Typography className=" fs_14 fw_600">Arrow Tooltip</Typography>
             <Tooltip title="Tooltip with arrow" arrow>
-              <Button
-                variant="contained"
-                color="secondary"
+              <button
+                className="btn_primary btn_primary_hover"
+                size="small"
+                style={{
+                  color: "white",
+                }}
                 //   className={classes.button}
               >
                 Arrow Tooltip
-              </Button>
+              </button>
             </Tooltip>
           </Grid>
           <Grid item xs={12} md={6} lg={6}>
@@ -129,13 +55,16 @@ const TooltipExample = () => {
               enterDelay={500}
               leaveDelay={200}
             >
-              <Button
-                variant="outlined"
-                color="primary"
+              <button
+                className="btn_primary btn_primary_hover"
+                size="small"
+                style={{
+                  color: "white",
+                }}
                 //   className={classes.button}
               >
                 Custom Delay Tooltip
-              </Button>
+              </button>
             </Tooltip>
           </Grid>
           <Grid item xs={12} md={6} lg={6}>
@@ -145,37 +74,42 @@ const TooltipExample = () => {
                 <Typography color="inherit">Tooltip with Typography</Typography>
               }
             >
-              <Button
-                variant="outlined"
-                color="secondary"
+              <button
+                className="btn_primary btn_primary_hover"
+                size="small"
+                style={{
+                  color: "white",
+                }}
                 //   className={classes.button}
               >
                 Typography Tooltip
-              </Button>
+              </button>
             </Tooltip>
           </Grid>
           <Grid item xs={12} md={6} lg={6}>
             <Typography className=" fs_14 fw_600">Disabled Tooltip</Typography>
             <Tooltip title="Disabled Tooltip" disabled>
-              <Button
-                variant="contained"
-                color="primary"
+              <button
+                className="btn_primary btn_primary_hover"
+                size="small"
+                style={{
+                  color: "white",
+                }}
                 //   className={classes.button}
               >
                 Disabled Tooltip
-              </Button>
+              </button>
             </Tooltip>
           </Grid>
-          <Grid item xs={12} md={6} lg={6}>
+          {/* <Grid item xs={12} md={6} lg={6}>
             <Typography className=" fs_14 fw_600">
               Tooltip on icon button
             </Typography>
             <Tooltip title="Tooltip on icon button">
               <IconButton aria-label="info">
-                {/* <InfoOutlined /> */}
               </IconButton>
             </Tooltip>
-          </Grid>
+          </Grid> */}
           <Grid item xs={12} md={6} lg={6}>
             <Typography className=" fs_14 fw_600">
               Tooltip on help icon
